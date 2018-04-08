81c20ac7fc99330963afd0fdbe84278510c8a7a3
狄文青 <wenqing.di@hand-china.com>
2018-04-08 16:18:45 +0800

    addfunction

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    public String handAMS()
+    {
+        String name="AMS一部";//杨恒
+        return name;
+    }
 }
